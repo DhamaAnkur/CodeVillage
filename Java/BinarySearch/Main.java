cb04e557884a1758a600d10d04a174e964153b39
DhamaAnkur <akdhama98@gmail.com>
2023-01-13 22:53:11 +0530

    changes

--- Java/BinarySearch/Main.java
+++ Java/BinarySearch/Main.java
@@ -36,7 +36,7 @@ public class Main {
         // for (int i = 0; i < n; i++) {
         //     arr[i] = scn.nextInt();
         // }
-
+//
 
         int []arr = {10,20,30,40,50,60};
         int k = 60;
