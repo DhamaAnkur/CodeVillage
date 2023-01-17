4ae6f64bd705f24edf40509c0c9106d315d46f75
DhamaAnkur <akdhama98@gmail.com>
2023-01-17 23:23:21 +0530

    ArmStrong

--- CVQuestions/Main.java
+++ CVQuestions/Main.java
@@ -65,7 +65,7 @@ public class Main {
 */
 
 //4. Write a Java Program for the Fibonacci series.
-
+/*
 import java.util.*;
 
 public class Main {
@@ -93,4 +93,83 @@ public class Main {
         }
 
     }
+}
+*/
+
+//5.Write a Java Program to find the duplicate characters in a string.
+/* 
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            for (int j = i+1; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                 System.out.println(str.charAt(j));
+                 return ; 
+                }
+            }
+
+        }
+
+    
+    }
+
+}
+*/
+
+
+//6.Write a Java Program to check Armstrong number. or in given range
+import java.util.*;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner scn =  new Scanner(System.in);
+        System.out.println("Enter the range");
+        int a = scn.nextInt();
+        int z = scn.nextInt();
+     
+        for(int i=a ; i<=z ; i++){
+         if(IsArmStrong(i)){
+          System.out.println("ArmStrong-->" + i);
+        }
+    }
+
+    }
+
+    public static boolean IsArmStrong(int num){
+      int originalNumber = num ;
+      int count = 0;
+      int sum = 0;
+
+      while(originalNumber!=0){
+        originalNumber /= 10;
+        count++;
+      }
+
+      originalNumber = num ;
+
+      while(originalNumber != 0){
+        int rem =  originalNumber%10 ;
+        sum += Math.pow(rem, count);
+        originalNumber /= 10 ;
+      }
+
+      if(sum == num){
+        return true;
+      }
+
+        return false;
+      
+    }
+
+
 }
