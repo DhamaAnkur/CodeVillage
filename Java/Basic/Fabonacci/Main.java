58956ec2f5d2007c062e336a2ec1d9d9cd8ea79b
DhamaAnkur <akdhama98@gmail.com>
2023-01-23 14:18:22 +0530

    primeNumber

--- Java/Basic/Fabonacci/Main.java
+++ Java/Basic/Fabonacci/Main.java
@@ -3,31 +3,32 @@
 import java.util.*;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int a=0;
-        int b=1;
+        int a = 0;
+        int b = 1;
         int c = 0;
 
-        if(n==1){
-            System.out.println("Nth Fibonacci-->" +0);
-            return ;
-        }
-        else if(n==2){
-            System.out.println("Nth Fibonacci-->" +1);
-            return ;
-        }
-        else{
-        for(int i=0; i<n-2; i++){
-            c = a+b;
-            a = b;
-            b = c;
+        if (n == 1) {
+            System.out.println("Nth Fibonacci-->" + a);
+            return;
         }
 
-        System.out.print("Nth Fibonacci-->" + c);
+        else if (n == 2) {
+            System.out.println("Nth Fibonacci-->" + b);
+            return;
+        }
 
-    }
+        else {
+            for (int i = 2; i < n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.print("Nth Fibonacci-->" + c);
+        }
+
     }
 }
---
+++ Java/Basic/PrimeNumber/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+    
+    public static void main(String[] args) {
+           Scanner scn = new Scanner(System.in);
+
+           int n = 874522467 ;
+
+           for(int i=2; i<=n/2 ; i++){
+            if((n%i) == 0){
+                System.out.println("Not a prime Number");
+                return ;
+            }
+           }
+
+           System.out.println("This is a prime number");
+    }
+
+}
