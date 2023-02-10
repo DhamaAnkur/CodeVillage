d70ec17756939a0b521b2956da8d70d95f89333a
DhamaAnkur <akdhama98@gmail.com>
2023-02-10 18:44:24 +0530

    RemoveVowel

--- Questions/RemoveVowel/Main.java
+++ Questions/RemoveVowel/Main.java
@@ -4,17 +4,34 @@ import java.util.*;
 
 public class Main {
 
-    public static void removeVowel(String name  , char[]ch){
+    public static void removeVowel(String name) {
+        char[] ch = { 'a', 'e', 'i', 'o', 'u' };
+        String ans = "";
+
+        for (int i = 0; i < name.length(); i++) {
+            int j = 0;
+
+            while (j < ch.length) {
+                if (name.charAt(i) == ch[j]) {
+                    break;
+                }
+                j++;
+            }
+
+            if (j == ch.length) {
+                ans += name.charAt(i);
+            }
+            
+        }
+
+        System.out.println(ans);
 
     }
 
     public static void main(String[] args) {
         String name = "ankurdhama";
-        char [] ch = {'a', 'e' , 'i' , 'o', 'u'};
-        // removeVowel(name ,ch);
-        System.out.println(ans);
-
+        removeVowel(name);
 
     }
-    
+
 }
