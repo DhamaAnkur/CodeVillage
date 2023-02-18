50018ab32d9102f2c1a247a73f5ef0dbd9cbe95c
DhamaAnkur <akdhama98@gmail.com>
2023-02-19 00:15:26 +0530

    Polymorphism

--- OOPS/OverLoading/Main.java
+++ OOPS/OverLoading/Main.java
@@ -2,18 +2,18 @@
 
 // Over Loading
 class A {
-    void s(String a) {
-        System.out.println("Ankur");
+    void show(Object a) {
+        System.out.println("From method show1");
 
     }
 
-    void s(int n) {
-        System.out.println(11);
+    void show(int n) {
+        System.out.println("From method show2");
 
     }
 
-    void s(int n, String a) {
-        System.out.println("11 Ankur");
+    void show(int n, String a) {
+        System.out.println("From method show3");
     }
 
 }
@@ -22,7 +22,10 @@ public class Main {
 
     public static void main(String[] args) {
         A input = new A();
-        input.s( 5);
+        int [] arr  = new int[10];
+        input.show(input);  // it calls method 1-->show(object) because input is the object..
+        //input.show(arr) --> it calls method 1-->show(object) because object is the parent of all class 
+        //input.s('a');    automatic promote to integer and call-->show(int)
     }
 
 }
--- OOPS/OverRiding/Main.java
+++ OOPS/OverRiding/Main.java
@@ -1,30 +1,110 @@
-// package OOPS.OverRiding;
+// // Over Riding
+// class A{
+//     void show(){
+//         System.err.println("1");;
+//     } 
+// }
 
-// Over Riding
-class A{
-    void s(){
-        System.err.println("11");;
-    } 
-}
+// class B extends A{
+//     void show(){
+//         System.err.println("2");;
+//     } 
+// }
 
-class B extends A{
-    void sr(){
-        System.err.println("21");;
-    } 
-}
+// class C extends A{
+//     void show(){
+//         System.err.println("3");;
+//     } 
+// }
 
-class C extends A{
-    void src(){
-        System.err.println("31");;
-    } 
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         C input = new C();
+//         input.show(); 
+//     }
+// }
 
-public class Main {
+//----------------------------------------------------------------------------------------------------------------//
 
-    public static void main(String[] args) {
-        C input = new C();
-        input.s();
+//-----It is possoble to have different return type for a override method in child class but child's return 
+//type should be sub-type of parent return type .This phenomenon is known as covairant return type.:-
+
+// class abc {
+//     Object show(){
+//         System.out.println("from abc");
+//         return null;
+//     }
+// }
+
+// class xyz extends abc{
+//     String show(){
+//         System.out.println("from xyz");
+//         return null;
+//     }
+
+//     public static void main(String[] args) {
+//         abc ab = new abc();
+//         ab.show();
+
+//         xyz xy = new xyz();
+//         xy.show();
+//     }
+
+// }
+
+//---------------------------------------------------------------------------------------------------------------//
+//----Access modifier:-
+
+// class abc {
+//      void show(){
+//         System.out.println("from abc");
+       
+//     }
+// }
+
+// class xyz extends abc{
+//     public void show(){
+//         System.out.println("from xyz");
         
-    }
+//     }
+
+//     public static void main(String[] args) {
+//         abc ab = new abc();
+//         ab.show();
+
+//         xyz xy = new xyz();
+//         xy.show();
+//     }
+
+// }
+
+//--------------------------------------------------------------------------------------------------------------//
+//----Exception handling with overridden:-
+// (1)Ist case --> if parent class does throw any exception then child class only throws unchecked exception.
+
+// (2)2nd case --> in this case if child class have throw any exception then parent class must throw parent 
+//----------------> exception compared to child exception.
+
+class abc {
+    void show() throws RuntimeException{
+       System.out.println("from abc");
+      
+   }
+}
+
+class xyz extends abc{
+    void show() throws ArithmeticException{
+       System.out.println("from xyz");
+       
+   }
+
+   public static void main(String[] args) {
+       abc ab = new abc();
+       ab.show();
+
+       xyz xy = new xyz();
+       xy.show();
+   }
 
 }
+
