d1b37dd3ffe4a7d47ec1b7e5c059ab9d81462088
DhamaAnkur <akdhama98@gmail.com>
2023-02-24 18:17:56 +0530

    short_Keywords

---
+++ ALL_SHORT_KEYWORDS/Main.java
@@ -0,0 +1,72 @@
+// Ctrl + A - Select All
+// Ctrl + B - Bold
+// Ctrl + C - Copy
+// Ctrl + D - Fill
+// Ctrl + F - Find
+// Ctrl + G - Find next instance of text
+// Ctrl + H - Replace
+// Ctrl + I - Italic
+// Ctrl + K - Insert a hyperlink
+// Ctrl + N - New workbook
+// Ctrl + O - Open
+// Ctrl + P - Print
+// Ctrl + R - Nothing right
+// Ctrl + S - Save
+// Ctrl + U - Underlined
+// Ctrl + V - Paste
+// Ctrl +W - Close
+// Ctrl + X - Cut
+// Ctrl + Y - Repeat
+// Ctrl + Z - Cancel
+// F1 - Help
+// F2 - Edition
+// F3 - Paste the name
+// F4 - Repeat the last action
+// F4 - When entering a formula, switch between absolute / relative references
+// F5 - Goto
+// F6 - Next Pane
+// F7 - Spell Check
+// F8 - Extension of the mode
+// F9 - Recalculate all workbooks
+// F10 - Activate Menubar
+// F11 - New graph
+// F12 - Save As
+// Ctrl +: - Insert the current time
+// Ctrl +; - Insert the current date
+// Ctrl + "- Copy the value of the cell above
+// Ctrl + '- Copy the formula from the cell above Shift - Offset Adjustment for Additional Functions in the Excel Menu
+// Shift + F1 - What is it?
+// Shift + F2 - Edit cell comment
+// Shift + F3 - Paste the function into the formula Shift + F4 - Search Next
+// Shift + F5 - Find
+// Shift + F6 - Previous Panel
+// Shift + F8 - Add to the selection
+// Shift + F9 - Calculate the active worksheet
+// Shift + F10 - Popup menu display
+// Shift + F11 - New spreadsheet
+// Shift + F12 - Save
+// Ctrl + F3 - Set name
+// Ctrl + F4 - Close
+// Ctrl + F5 - XL, size of the restore window
+// Ctrl + F6 - Next Workbook Window
+// Shift + Ctrl + F6 - Previous Workbook Window Ctrl + F7 - Move window
+// Ctrl + F8 - Resize Window
+// Ctrl + F9 - Minimize the workbook
+// Ctrl + F10 - Maximize or Restore Window
+// Ctrl + F11 - Inset 4.0 Macro sheet
+// Ctrl + F1 - Open File
+// Alt + F1 - Insert a graph
+// Alt + F2 - Save As
+// Alt + F4 - Output
+// Alt + F8 - Macro dialog
+// Alt + F11 - Visual Basic Editor
+// Ctrl + Shift + F3 - Create a name using the names of row and column labels
+// Ctrl + Shift + F6 - Previous Window
+// Ctrl + Shift + F12 - Printing
+// Alt + Shift + F1 - New spreadsheet
+// Alt + Shift + F2 - Save
+// Alt + = - AutoSum
+// Ctrl + `- Toggle value / display of the formula Ctrl + Shift + A - Insert the argument names in the formula
+// Alt + down arrow - automatic view format
+// Alt + '- Format Style Dialog
+// Ctrl + Shift + ~ - General Format
