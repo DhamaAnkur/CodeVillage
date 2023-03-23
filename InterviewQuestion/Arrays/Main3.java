6e3ad2de25c862e3506d5d2854c276793c67c31e
DhamaAnkur <akdhama98@gmail.com>
2023-03-23 18:54:29 +0530

    Spiral

---
+++ InterviewQuestion/Arrays/Main3.java
@@ -0,0 +1,64 @@
+
+import java.util.*;
+
+public class Main3 {
+
+    public static Scanner scn = new Scanner(System.in);
+
+    public static void ColumnWiseSpiral(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+        int minCol = 0;
+        int minRow = 0;
+        int maxRow = n;
+        int count = 0;
+        List<Integer> al = new ArrayList<>();
+
+        while (count < arr.length * arr[0].length) {
+            if (minRow == 0) {
+                for (int i = minRow; i < maxRow && count < n * m; i++) {
+                    al.add(arr[i][minCol]);
+                    count++;
+                }
+                minCol++;
+            }
+
+            else {
+                for (int i = maxRow; i < minRow && count < n * m; i++) {
+                    al.add(arr[i][minCol]);
+                    count++;
+                }
+                minCol++;
+            }
+        }
+
+        System.out.println(al);
+
+    }
+
+    public static void Input2DArray(int[][] arr, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+    }
+
+    public static void print2DArray(int[][] arr, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        Input2DArray(arr, n, m);
+        ColumnWiseSpiral(arr);
+
+    }
+}
